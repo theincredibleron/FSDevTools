7ecee8e572f30a057497020824ecebbaf3954d7e
Nicolai Henczi <henczi@e-spirit.com>
2018-08-17 11:42:24 +0200

    DEVEX-188 added testing

--- fsdevtools-sharedutils/src/main/java/com/espirit/moddev/shared/StringUtils.java
+++ fsdevtools-sharedutils/src/main/java/com/espirit/moddev/shared/StringUtils.java
@@ -13,11 +13,11 @@ public class StringUtils {
      * @return true if the String is null or empty, false otherwise
      */
     public static boolean isNullOrEmpty(final String string) {
-        return string == null || isEmpty(string);
+        return string == null || string.trim().isEmpty();
     }
 
     /**
-     * @param string String to check
+     * @param string String to check (may not be null!)
      * @return true if the String is empty, false otherwise
      */
     public static boolean isEmpty(final String string) {
